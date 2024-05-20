a3cc1ba570b21c34d8ebf57a97042b33de1acb8e
Nicolas Masson <98894729+MassonNicolas44@users.noreply.github.com>
2024-05-19 22:19:00 -0300

    .

--- app/Models/TIpo.php
+++ app/Models/TIpo.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TIpo extends Model
+class Tipo extends Model
 {
     use HasFactory;
 
